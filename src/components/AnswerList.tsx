b354080e4cb4a3e5ef7cba2a7076f0f63ba0064c
coskuntekin <mail@coskuntekin.com>
2025-06-30 00:00:41 +0300

    Add QuestionList component to display survey answers

---
+++ src/components/AnswerList.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useSurveyDispatch } from "../context/SurveyContext";
+import type { Question } from "../types/survey";
+import AnswerCard from "./AnswerCard";
+
+interface QuestionListProps {
+  questions: Question[];
+}
+
+const QuestionList: React.FC<QuestionListProps> = ({ questions }) => {
+  const dispatch = useSurveyDispatch();
+
+  return (
+    <div>
+      <h3 className="mb-2 font-semibold">Answers</h3>
+      {questions.length === 0 ? (
+        <div className="text-gray-500">No answers added yet.</div>
+      ) : (
+        questions.map((question) => (
+          <AnswerCard
+            key={question.id}
+            question={question}
+            questionCount={questions.length}
+            onChangeText={(text) =>
+              dispatch({
+                type: "updateQuestion",
+                payload: { questionId: question.id, text },
+              })
+            }
+            onDuplicate={() =>
+              dispatch({
+                type: "duplicateQuestion",
+                payload: { questionId: question.id },
+              })
+            }
+            onDelete={() =>
+              dispatch({
+                type: "deleteQuestion",
+                payload: { questionId: question.id },
+              })
+            }
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default QuestionList;

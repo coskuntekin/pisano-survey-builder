62aa16af7e7b3ffde19b5b81376b9b37f8d737e9
coskuntekin <mail@coskuntekin.com>
2025-06-30 14:36:50 +0300

    Refactor AppLayout component to include surveyId prop in Header and streamline structure

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -2,11 +2,27 @@ import { Outlet, useNavigate } from "react-router-dom";
 import { useAuthStore } from "../stores/auth";
 import { useEffect } from "react";
 import Header from "../components/Header";
-import { SurveyProvider } from "../context/SurveyContext";
+import { SurveyProvider, useSurveyState } from "../context/SurveyContext";
 
 export default function AppLayout() {
   const { user, logout, isLoading } = useAuthStore();
   const navigate = useNavigate();
+
+  return (
+    <SurveyProvider>
+      <AppLayoutContent
+        user={user}
+        logout={logout}
+        isLoading={isLoading}
+        navigate={navigate}
+      />
+    </SurveyProvider>
+  );
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function AppLayoutContent({ user, logout, isLoading, navigate }: any) {
+  const surveyState = useSurveyState();
 
   useEffect(() => {
     if (!user && !isLoading) {
@@ -16,11 +32,14 @@ export default function AppLayout() {
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
-      <Header user={user} logout={logout} isLoading={isLoading} />
+      <Header
+        user={user}
+        logout={logout}
+        isLoading={isLoading}
+        surveyId={surveyState.id}
+      />
       <main className="flex-1 p-8">
-        <SurveyProvider>
-          <Outlet />
-        </SurveyProvider>
+        <Outlet />
       </main>
     </div>
   );

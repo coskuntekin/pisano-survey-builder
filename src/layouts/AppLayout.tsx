0ed0b9993462de0eb0c312ec3accee32330fd98d
coskuntekin <mail@coskuntekin.com>
2025-06-30 15:41:58 +0300

    Remove unused survey state from AppLayout and simplify Header component props

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet, useNavigate } from "react-router-dom";
 import { useAuthStore } from "../stores/auth";
 import { useEffect } from "react";
 import Header from "../components/Header";
-import { SurveyProvider, useSurveyState } from "../context/SurveyContext";
+import { SurveyProvider } from "../context/SurveyContext";
 
 export default function AppLayout() {
   const { user, logout, isLoading } = useAuthStore();
@@ -22,8 +22,6 @@ export default function AppLayout() {
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function AppLayoutContent({ user, logout, isLoading, navigate }: any) {
-  const surveyState = useSurveyState();
-
   useEffect(() => {
     if (!user && !isLoading) {
       navigate("/login");
@@ -32,12 +30,7 @@ function AppLayoutContent({ user, logout, isLoading, navigate }: any) {
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
-      <Header
-        user={user}
-        logout={logout}
-        isLoading={isLoading}
-        surveyId={surveyState.id}
-      />
+      <Header user={user} logout={logout} isLoading={isLoading} />
       <main className="flex-1 p-8">
         <Outlet />
       </main>
